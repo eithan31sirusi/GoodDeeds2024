c87f91cc0a81e5b9e7813dfbf56442bc81356b52
eithan31sirusi <eithan26sirusi@gmail.com>
2024-01-10 23:40:10 +0200

    Updated Footer for sticky positioning and added content

--- client/src/layout/Footer/style.js
+++ client/src/layout/Footer/style.js
@@ -1,11 +1,52 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const FooterContainer = styled.footer`
-    background-color: var(--grey);
-    color: var(--deepBlue);
-    text-align: center;
-    padding: 10px;
-    position: absolute;
-    bottom: 0;
-    width: 100%;
+  background-color: var(--grey);
+  color: var(--deepBlue);
+  display: flex;
+  justify-content: space-around;
+  padding: 20px;
+  flex-wrap: wrap;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+export const FooterColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+`;
+
+export const SocialLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+
+  svg {
+    fill: var(--deepBlue);
+    cursor: pointer;
+    width: 20px; // Control the size of the icons here
+    height: 20px; // Ensure icons are square or adjust as needed
+
+    &:hover {
+      fill: var(--skyBlue);
+    }
+  }
+`;
+
+export const Info = styled.p`
+  text-align: center;
+`;
+
+export const LinkItem = styled(Link)`
+  color: var(--deepBlue);
+  text-decoration: none;
+  margin-bottom: 10px;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
